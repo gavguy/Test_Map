279a49697395865d2c3390664faac5a1c003e01f
Andrej_Tesakov <gavguy3@gmail.com>
2018-10-16 18:20:32 +0300

    sozdan spisok text_note_map

--- src/notepad_1_gav/Main.java
+++ src/notepad_1_gav/Main.java
@@ -1,6 +1,7 @@
 package notepad_1_gav;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 
@@ -8,11 +9,24 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        Map<String, String> translation = new HashMap<>();
+       // Map<String, String> translation = new HashMap<>();
+        Map<String, String> translation = new LinkedHashMap<>(); //svjazanij hasmap- porjadok elementov sohranjon
         translation.put("one", "один");
         translation.put("home", "дом");
         translation.put("mouse", "мышь");
 
         System.out.println(translation.get("home"));
+
+        for (String k : translation.keySet()) {
+            System.out.printf("%s -> %s\n", k, translation.get(k));  //%s vivesti stroku \n na novuju stroku perevod
+        }
+
+
+
+
+
+
+
+
     }
 }
